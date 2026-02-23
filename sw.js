c9950fe7c2102e13e9761b5d038b6b3b13b68d94
aphio <56746969+aphioluiz@users.noreply.github.com>
2026-02-23 01:29:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,22 @@
-const CACHE_NAME = "calculadora-v2";
+const CACHE_NAME = "calculadora-cache-v1";
+
+const urlsToCache = [
+  "/calculadora/",
+  "/calculadora/index.html",
+  "/calculadora/manifest.json",
+  "/calculadora/icone.png"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
-  // NÃO intercepta requisições externas (API)
-  if (!event.request.url.startsWith(self.location.origin)) return;
-
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
